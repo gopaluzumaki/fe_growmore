5ea3c84dcbf15f864ba86ca3a1f72e2f5a5e9bd5
aakash <aakashrc.pune@gmail.com>
2024-11-05 22:49:29 +0530

    Sub title related chanegs

--- src/components/AddLeads.tsx
+++ src/components/AddLeads.tsx
@@ -117,6 +117,11 @@ const AddLeads = () => {
         <div className={`flex-grow ml-80 my-5 px-2`}>
           <div className="my-5 px-2 ">
             <Header />
+            <div className="flex">
+              <p className="text-[#7C8DB5] mt-1.5 ml-1">
+              {'Lead > Add New'} 
+              </p>
+            </div>
             <div>
               <div className="my-4 p-6 border border-[#E6EDFF] rounded-xl">
                 <form onSubmit={handleSubmit}>
